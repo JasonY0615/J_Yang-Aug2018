69ea1af82aabea2359892bbdd67d2df4cd913714
APCS2nd <APCS2nd@MHS-Math2-28>
2018-08-20 09:45:51 -0700

    First project to commit to GitHub for 2018

---
+++ Hello_world/src/Hello_world.java
@@ -0,0 +1,16 @@
+/**
+ * Print out a message saying Hello! 
+ * 
+ * @author Jason Yang  
+ * @version 8/17/2018
+ */
+public class Hello_world
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World! From Jason Yang"); //Display the string}
+    }
+} 
+
+	
+
