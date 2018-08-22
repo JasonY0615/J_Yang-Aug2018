764f6082b03502a560b3830afa0e14fff62e3d8e
APCS2nd <APCS2nd@MHS-Math2-28>
2018-08-22 10:49:36 -0700

    version2.0 Hello World

--- Hello_world/src/Hello_world.java
+++ Hello_world/src/Hello_world.java
@@ -1,6 +1,5 @@
 /**
  * Print out a message saying Hello! 
- * 
  * @author Jason Yang  
  * @version 8/17/2018
  */
@@ -8,7 +7,7 @@ public class Hello_world
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello World! From Jason Yang"); //Display the string}
+        System.out.println("Hello World! From Jason Yang"); //Display the string
     }
 } 
 
